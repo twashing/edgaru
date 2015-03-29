d7cd64100e9d26941bf348cec4ef1dc3d829adc4
Timothy Washington <twashing@interruptsoftware.com>
2015-03-29 01:21:29 +0000

    -- reworking generate-timeseries and generate-prices to return lazy sequences

--- src/edgaru/core.clj
+++ src/edgaru/core.clj
@@ -12,34 +12,21 @@
 
 (defn generate-timeseries [pricelist]
 
-  (->> (zipmap (map (fn [x] {:time x}) (take (count pricelist) (iterate inc 0)))
-               (map (fn [x] {:price x}) pricelist))
-
-       (map (fn [x] (merge (first x) (second x))))
-
-       (sort-by :time)))
+  (->> (map (fn [x y] [x y]) (map (fn [x] {:time x}) (iterate inc 0))
+            (map (fn [x] {:price x}) pricelist))
+       (map (fn [x] (merge (first x) (second x))))))
 
 (comment
 
-  ;; generate a raw list of floats within a given range
-  (def plist (take 10 (generate-prices 5 15)))
+  ;; generate a raw infinite list of floats within a given range
+  (def pricelist (generate-prices 5 15))
 
-  ;; => (14.425231029100223 12.561596873601552 9.015979284812179 7.20001565607321 5.001856661709986 11.495735016910736 8.726872997660886 14.28593925643085 6.002666920761813 10.198040792662994)
+  ;; => (14.425231029100223 12.561596873601552 9.015979284812179 7.20001565607321 5.001856661709986 11.495735016910736 8.726872997660886 14.28593925643085 6.002666920761813 10.198040792662994 ...)
 
 
   ;; generate a timeseries based on thoe numbers
-  (def tseries (generate-timeseries plist))
+  (def timeseries (take 10 (generate-timeseries pricelist)))
 
-  ;; =>
-  #_({:price 6.498531167254335, :time 0}
-     {:price 14.932585251457299, :time 1}
-     {:price 7.1285417453458795, :time 2}
-     {:price 12.81164293198097, :time 3}
-     {:price 7.244480692160645, :time 4}
-     {:price 13.19382349693832, :time 5}
-     {:price 6.261360701619896, :time 6}
-     {:price 10.569452494512952, :time 7}
-     {:price 9.851295409730925, :time 8}
-     {:price 6.778911488317861, :time 9})
+  ;; => ({:price 6.498531167254335, :time 0} {:price 14.932585251457299, :time 1} {:price 7.1285417453458795, :time 2} {:price 12.81164293198097, :time 3} {:price 7.244480692160645, :time 4} {:price 13.19382349693832, :time 5} {:price 6.261360701619896, :time 6} {:price 10.569452494512952, :time 7} {:price 9.851295409730925, :time 8} {:price 6.778911488317861, :time 9})
 
   )
