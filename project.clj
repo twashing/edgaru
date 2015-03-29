5ab6504208e2fae5afb5933aa0e3d5cd8eab2319
Timothy Washington <twashing@interruptsoftware.com>
2015-03-29 21:32:05 +0000

    -- for timeseries, defining random time intervals within 4 seconds

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject edgaru "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description ""
+  :url "https://github.com/twashing/edgaru"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-time "0.9.0"]]
+  :profiles {:dev {:dependencies [[alembic "0.3.2"]]}})
--- src/edgaru/core.clj
+++ src/edgaru/core.clj
@@ -1,20 +1,31 @@
 (ns edgaru.core
-  (:require [clojure.pprint :as pp]))
+  (:require [alembic.still]
+            [clojure.pprint :as pp]
+            [clj-time.core :as t]
+            [clj-time.periodic :as p]))
+
+
+(defn reload-project []
+  (alembic.still/load-project))
 
 (defn generate-prices
 
   ([upper-bound]
-   (generate-timeseries 0 upper-bound))
+   (generate-prices 0 upper-bound))
 
   ([lower-bound upper-bound]
    (filter #(>= % lower-bound)
            (repeatedly #(rand upper-bound)))))
 
-(defn generate-timeseries [pricelist]
+(defn generate-timeseries
 
-  (->> (map (fn [x y] [x y]) (map (fn [x] {:time x}) (iterate inc 0))
-            (map (fn [x] {:price x}) pricelist))
-       (map (fn [x] (merge (first x) (second x))))))
+  ([pricelist]
+   (generate-timeseries pricelist (t/now)))
+
+  ([pricelist datetime]
+   (->> (map (fn [x y] [x y]) (map (fn [x] {:time x}) (iterate #(t/plus % (t/seconds (rand 4))) datetime))
+             (map (fn [x] {:price x}) pricelist))
+        (map (fn [x] (merge (first x) (second x)))))))
 
 (comment
 
@@ -28,5 +39,23 @@
   (def timeseries (take 10 (generate-timeseries pricelist)))
 
   ;; => ({:price 6.498531167254335, :time 0} {:price 14.932585251457299, :time 1} {:price 7.1285417453458795, :time 2} {:price 12.81164293198097, :time 3} {:price 7.244480692160645, :time 4} {:price 13.19382349693832, :time 5} {:price 6.261360701619896, :time 6} {:price 10.569452494512952, :time 7} {:price 9.851295409730925, :time 8} {:price 6.778911488317861, :time 9})
+
+
+  (def one (t/now))
+
+  (def two (t/plus one (t/seconds 1)))
+
+  (let [start (System/nanoTime)
+        q (seque
+           (iterate
+            #(do (Thread/sleep 400) (inc %))
+            0))]
+
+    (println "sleep five seconds...")
+    (Thread/sleep 5000)
+
+    (doseq [i (take 20 q)]
+      (println (int (/ (- (System/nanoTime) start) 1e7))
+               ":" i)))
 
   )
