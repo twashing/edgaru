48d502f0967fe67f601aaf3d2dfaae1e02078a5b
Timothy Washington <twashing@interruptsoftware.com>
2015-06-27 18:29:55 +0000

    -- churn on Chapter 4

--- src/edgaru/four.clj
+++ src/edgaru/four.clj
@@ -1,30 +1,140 @@
 (ns edgaru.four
-  (:require [edgaru.three :as thr]))
+  (:require [edgaru.two :as two]
+            [edgaru.three :as three]))
+
+(declare timeseries)
+(def price-list (two/generate-prices 5 15))
 
 
-(reduce (fn [rslt ech]
+'({:last 10.978625695681702, :lows [5], :highs [15]}
+ {:last 15.393542022616002,
+  :lows (5 5),
+  :highs (15 15.393542022616002)}
+ {:last 15.68497326182313,
+  :lows (5 5 5),
+  :highs (15.68497326182313 15 15.393542022616002)}
+ {:last 17.894866781714637,
+  :lows (5 5 5 5),
+  :highs (17.894866781714637 15.68497326182313 15 15.393542022616002)}
+ {:last 19.178454686228328,
+  :lows (5 5 5 5 5),
+  :highs
+  (19.178454686228328
+   17.894866781714637
+   15.68497326182313
+   15
+   15.393542022616002)})
 
-                     (let [etal-keys [:last-trade-price :last-trade-time]
-                           output-key :output
-                           tsum (reduce (fn [rslt inp]
+(defn extract-price-only [price-list]
+  (map :last price-list))
 
-                                          (println (str rslt " <==> " inp))
-                                          (let [ltprice (:last (:last-trade-price inp))]
-                                            (+ ltprice rslt))) 0 ech)
-                           taverage (/ tsum (count ech)) ]
+(def price-only-list (extract-price-only (two/generate-prices 5 15)))
 
-                       (cons (merge
-
-                              (zipmap etal-keys
-                                      (map #(% (first ech)) etal-keys))
+'(10.978625695681702
+  15.393542022616002
+  15.68497326182313
+  17.894866781714637
+  19.178454686228328)
 
 
-                              {output-key taverage
-                               :population ech})
-                             rslt)))
+(def time-series (two/generate-timeseries price-only-list))
 
-                   (into '() (repeat 20 nil))
-                   (reverse (partition 20 1 timeseries)))
+'({:last-trade-price 10.774174002394385,
+   :last-trade-time #<DateTime 2015-06-27T17:54:37.583Z>}
+  {:last-trade-price 6.221195542189912,
+   :last-trade-time #<DateTime 2015-06-27T17:54:40.583Z>}
+  {:last-trade-price 6.98092516851132,
+   :last-trade-time #<DateTime 2015-06-27T17:54:40.583Z>}
+  {:last-trade-price 5.5980561319315,
+   :last-trade-time #<DateTime 2015-06-27T17:54:40.583Z>}
+  {:last-trade-price 5.263260952271663,
+   :last-trade-time #<DateTime 2015-06-27T17:54:42.583Z>})
+
+
+(partition 20 1 (range 40))
+'((0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
+ (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+ (2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21)
+ (3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22)
+ (4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23)
+ (5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24)
+ (6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25)
+ (7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26)
+ (8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27)
+ (9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28)
+ (10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29)
+ (11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30)
+ (12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31)
+ (13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32)
+ (14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33)
+ (15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34)
+ (16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35)
+ (17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36)
+ (18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37)
+ (19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38)
+ (20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39))
+
+
+(defn simple-moving-average
+  [options tick-window tick-list]
+
+  (let [start-index tick-window
+
+        {input-key :input
+         output-key :output
+         etal-keys :etal
+         :or {input-key :last-trade-price
+              output-key :last-trade-price-average
+              etal-keys [:last-trade-price :last-trade-time]}} options]
+
+    (reduce (fn [rslt ech]
+
+              (let [tsum (reduce (fn [rslt ech]
+                                   (let [ltprice (:last-trade-price ech)]
+                                     (+ ltprice rslt))) 0 ech)
+
+                    taverage (/ tsum (count ech))]
+
+                #spy/d ech
+                (lazy-cat rslt
+
+                          (merge
+
+                           (zipmap etal-keys
+                                   (map #(% ech) etal-keys))
+
+                           '({output-key taverage
+                               :population ech})))))
+            '()
+            (partition tick-window
+                       1
+                       (take (* 2 tick-window) tick-list)))))
+
+
+(comment (reduce (fn [rslt ech]
+
+                   (let [etal-keys [:last-trade-price :last-trade-time]
+                         output-key :output
+                         tsum (reduce (fn [rslt inp]
+
+                                        (println (str rslt " <==> " inp))
+                                        (let [ltprice (:last (:last-trade-price inp))]
+                                          (+ ltprice rslt))) 0 ech)
+                         taverage (/ tsum (count ech)) ]
+
+                     (cons (merge
+
+                            (zipmap etal-keys
+                                    (map #(% (first ech)) etal-keys))
+
+
+                            {output-key taverage
+                             :population ech})
+                           rslt)))
+
+                 (into '() (repeat 20 nil))
+                 (reverse (partition 20 1 timeseries))))
+
 
 (defn exponential-moving-average
   "From a tick-list, generates an accompanying exponential moving average list.
@@ -41,7 +151,7 @@
 
   ([options tick-window tick-list]
 
-   (exponential-moving-average options tick-window tick-list (thr/simple-moving-average nil tick-window tick-list)))
+   (exponential-moving-average options tick-window tick-list (three/simple-moving-average nil tick-window tick-list)))
 
   ([options tick-window tick-list sma-list]
 
@@ -102,7 +212,7 @@
   ** This function assumes the latest tick is on the left**"
 
   ([tick-window tick-list]
-   (bollinger-band tick-window tick-list (thr/simple-moving-average nil tick-window tick-list)))
+   (bollinger-band tick-window tick-list (three/simple-moving-average nil tick-window tick-list)))
 
   ([tick-window tick-list sma-list]
 
