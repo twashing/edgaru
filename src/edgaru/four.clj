4ab4d8adee2562618503239d90b913029c79c888
Timothy Washington <twashing@interruptsoftware.com>
2015-06-28 03:13:48 +0000

    -- cleaning up the bollinger-band function

--- src/edgaru/four.clj
+++ src/edgaru/four.clj
@@ -189,22 +189,13 @@
              '()
              sma-list))))
 
-(comment
-
-  (def price-only-list (extract-price-only (two/generate-prices 5 15)))
-  (def time-series (two/generate-timeseries price-only-list))
-
-  (def sma-list (simple-moving-average {} 20 time-series))
-  (def ema-list (exponential-moving-average {} 20 time-series sma-list))
-
-  )
 
 (defn bollinger-band
   "From a tick-list, generates an accompanying list with upper-band and lower-band
-     Upper Band: K times an N-period standard deviation above the moving average (MA + Kσ)
-     Lower Band: K times an N-period standard deviation below the moving average (MA − Kσ)
-     K: number of standard deviations
-     N: period, or tick-window we are looking at
+  Upper Band: K times an N-period standard deviation above the moving average (MA + Kσ)
+  Lower Band: K times an N-period standard deviation below the moving average (MA − Kσ)
+  K: number of standard deviations
+  N: period, or tick-window we are looking at
   Returns a list, equal in length to the tick-list, but only with slots filled,
   where preceding tick-list allows.
   ** This function assumes the latest tick is on the left**"
@@ -215,42 +206,44 @@
   ([tick-window tick-list sma-list]
 
    ;; At each step, the Standard Deviation will be: the square root of the variance (average of the squared differences from the Mean)
+   (reduce (fn [rslt ech]
 
-   (let [bollinger-list (into '() (repeat tick-window nil))]
+             (let [;; get the Moving Average
+                   ma (:last-trade-price-average ech)
 
-     (reduce (fn [rslt ech]
+                   ;; work out the mean
+                   mean (/ (reduce (fn [rslt ech]
+                                     (+ (:last-trade-price ech)
+                                        rslt))
+                                   0
+                                   (:population ech))
+                           (count (:population ech)))
 
-               (let [;; get the Moving Average
-                     ma (:last-trade-price-average ech)
-
-                     ;; work out the mean
-                     mean (/ (reduce (fn [rslt ech]
-                                       (+ (if (string? (:last-trade-price ech))
-                                            (read-string (:last-trade-price ech))
-                                            (:last-trade-price ech))
-                                          rslt))
-                                     0
+                   ;; Then for each number: subtract the mean and square the result (the squared difference)
+                   sq-diff-list (map (fn [ech]
+                                       (let [diff (- mean (:last-trade-price ech))]
+                                         (* diff diff)))
                                      (:population ech))
-                             (count (:population ech)))
 
-                     ;; Then for each number: subtract the mean and square the result (the squared difference)
-                     sq-diff-list (map (fn [ech]
-                                         (let [diff (- mean (if (string? (:last-trade-price ech))
-                                                              (read-string (:last-trade-price ech))
-                                                              (:last-trade-price ech)))]
-                                           (* diff diff)))
-                                       (:population ech))
+                   variance (/ (reduce + sq-diff-list) (count (:population ech)))
+                   standard-deviation (. Math sqrt variance)]
 
-                     variance (/ (reduce + sq-diff-list) (count (:population ech)))
-                     standard-deviation (. Math sqrt variance)]
-                 (cons {:last-trade-price (:last-trade-price ech)
-                        :last-trade-time (:last-trade-time ech)
-                        :upper-band (+ ma (* 2 standard-deviation))
-                        :lower-band (- ma (* 2 standard-deviation))}
-                       rslt)))
-             bollinger-list
-             (->> sma-list (remove nil?) reverse)))))
+               (lazy-cat rslt
+                         [{:last-trade-price (:last-trade-price ech)
+                           :last-trade-time (:last-trade-time ech)
+                           :upper-band (+ ma (* 2 standard-deviation))
+                           :lower-band (- ma (* 2 standard-deviation))}])))
+           '()
+           sma-list)))
+
 
 (comment
 
-        )
+  (def price-only-list (extract-price-only (two/generate-prices 5 15)))
+  (def time-series (two/generate-timeseries price-only-list))
+
+  (def sma-list (simple-moving-average {} 20 time-series))
+  (def ema-list (exponential-moving-average {} 20 time-series sma-list))
+  (def bol-band (bollinger-band 20 time-series sma-list))
+
+  )
