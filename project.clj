547a5821caa1192a0bd075e5917255483aa9cf1c
Timothy Washington <twashing@interruptsoftware.com>
2015-07-09 04:02:19 +0000

    -- trying overtone's sine function

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.9.0"]
-                 [com.taoensso/timbre "3.4.0"]]
+                 [com.taoensso/timbre "3.4.0"]
+                 [overtone "0.9.1"]]
   :profiles {:dev {:dependencies [[alembic "0.3.2"]]}})
