edb7da6bb4f309f052efd437dbb6c8ae446e8651
Timothy Washington <twashing@interruptsoftware.com>
2015-07-11 17:51:34 +0000

    -- adding the polynomial exploratory function

--- src/edgaru/five.clj
+++ src/edgaru/five.clj
@@ -1,1 +1,16 @@
 (ns edgaru.five)
+
+
+  (defn polynomial [x]
+    (->
+     (+ (* 2
+           (Math/pow x 3))
+
+        (* 2
+           (Math/pow x 2)))
+     (- (* 3 x))))
+
+(comment
+
+
+  )
