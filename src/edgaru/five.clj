133500c7a74691717a7211a2db67797460df0374
Timothy Washington <twashing@interruptsoftware.com>
2015-07-12 19:09:32 +0000

    -- added sine function

--- src/edgaru/five.clj
+++ src/edgaru/five.clj
@@ -1,16 +1,35 @@
 (ns edgaru.five)
 
 
-  (defn polynomial [x]
-    (->
-     (+ (* 2
-           (Math/pow x 3))
+(defn polynomial [x]
+  (->
+   (+ (* 2
+         (Math/pow x 3))
 
-        (* 2
-           (Math/pow x 2)))
-     (- (* 3 x))))
+      (* 2
+         (Math/pow x 2)))
+   (- (* 3 x))))
+
+
+;; f(x) = a sin (b(x − c)) + d
+;; y = a sin (b(x − c)) + d
+;; y = a sin (b(x − pi/2)) + d
+
+;; d - quantifies vertical translation
+;; a - amplitude of the wave
+;; b - horizontal dilation
+;; c - horizontal translation
+
+(defn sine [a b x d]
+
+  (* a
+     (Math/sin (* b
+                  (- x
+                     (/ Math/PI 2)))))
+  )
+
+
 
 (comment
-
 
   )
