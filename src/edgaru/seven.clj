a4e35c908cacfbdfa99ec9f4b967fab42a49e622
Timothy Washington <twashing@interruptsoftware.com>
2015-08-09 22:00:20 +0000

    -- adding a build-system function -- adding send-data! and receive-data functions

--- src/edgaru/seven.clj
+++ src/edgaru/seven.clj
@@ -1,6 +1,6 @@
 (ns edgaru.seven
   (:require [clojure.java.io :as io]
-            [clojure.core.async :as async :refer [go <! >!]]
+            [clojure.core.async :as async :refer [go go-loop chan close! <! >!]]
             [com.stuartsierra.component :as component]
             [edgaru.two :as two]
             [edgaru.four :as four]
@@ -17,29 +17,78 @@
     (spit full-path (list (apply pr-str data))
            :encoding "UTF-8")))
 
+
+;; =====
+;; Component code
+
+(defn send-data! [channel time-series]
+
+  (go-loop [prices (take 320 time-series)
+            remaining (drop 320 time-series)]
+
+    (let [sma (four/simple-moving-average {} 20 prices)
+          ema (four/exponential-moving-average {} 20 prices sma)
+          bol (four/bollinger-band 20 prices sma)]
+
+      (>! channel {:ticks prices :sma sma :ema ema :bol bol})
+      (Thread/sleep 1000))
+
+    (recur (take 320 remaining)
+           (drop 320 remaining))))
+
+(defn receive-data! [channel]
+
+  (go-loop [data (<! channel)]
+            (println data)
+            (if-not (nil? data)
+              (recur (<! channel)))))
+
 (defrecord Timeseries []
   component/Lifecycle
 
   (start [component]
-    component)
+    (let [c (chan)
+          price-list (five/generate-prices)
+          time-series (two/generate-timeseries price-list)]
+
+      (send-data! c time-series)
+      (assoc component :channel c)))
 
   (stop [component]
-    component))
+    (let [c (:channel component)]
+      (close! c)
+      (dissoc component :channel))))
 
-(defrecord Consumer [channel]
+(defrecord Consumer [timeseries]
   component/Lifecycle
 
   (start [component]
-    component)
+    (let [channel (:channel timeseries)]
+
+      (receive-data! channel)
+      (assoc component :channel channel)))
 
   (stop [component]
-    component))
+    (dissoc component :channel)))
 
+
+;; ====
 (defn new-timeseries []
-  (map->Timeseries))
+  (map->Timeseries {}))
 
-(defn new-consumer [channel]
-  (map->Consumer {:channel channel}))
+(defn new-consumer []
+  (map->Consumer {}))
+
+
+;; ====
+(defn build-system []
+  (component/system-map
+   :tms (new-timeseries)
+   :cns (component/using
+              (new-consumer)
+              {:timeseries :tms})))
+
+(def system (build-system))
 
 
 ;; 1. call five/generate-prices (pull 320)
@@ -103,5 +152,10 @@
 
     (close! c))
 
+
+  ;; 6.
+  (alter-var-root #'system component/start)
+
+  (alter-var-root #'system component/stop)
 
   )
