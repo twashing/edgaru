93b08807260daed912f0f8d805008c5e74897acf
Timothy Washington <twashing@interruptsoftware.com>
2015-08-16 20:10:02 +0000

    -- first pass at larger lookup function

--- src/edgaru/eight.clj
+++ src/edgaru/eight.clj
@@ -1,5 +1,6 @@
 (ns edgaru.eight
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]))
 
 
 ;; 0. with-open explanation
@@ -49,7 +50,7 @@
 (comment
 
   (map (fn [x]
-         (let [read-fn (comp edn/read-string slurp)
+         (let [read-fn (comp edn/read-string slurp)   ;; -> use of `comp` function
                pred-fn #(= #inst "2015-08-15T17:18:51.352-00:00" (:last-trade-time %))
                inp-edn (read-fn x)]
 
@@ -86,7 +87,7 @@
 
 (comment
 
-  (defn lookup []
+  (defn lookupfn []
 
     (flatten
      (map (fn [x]
@@ -111,7 +112,7 @@
 
 (comment
 
-  (defn lookup [flist pred-fn]
+  (defn lookupfn [flist pred-fn]
 
     (flatten
      (map (fn [x]
@@ -121,8 +122,14 @@
                       inp-edn)))
           flist)))
 
-  (defn specific-time-pred [inst]
+  (defn specific-time-pred [inst]   ;; -> functions returning functions
     #(= inst (:last-trade-time %)))
+
+  (defn time-after-pred [time]
+    #(.after (:last-trade-time %) time))
+
+  (defn time-before-pred [time]
+    #(.before (:last-trade-time %) time))
 
   (defn time-range-pred [lower upper]
     #(and (.after (:last-trade-time %) lower)
@@ -131,6 +138,12 @@
   (defn specific-price-pred [price]
     #(= price (:last-trade-price %)))
 
+  (defn price-abouve-pred [price]
+    #(> (:last-trade-price %) price))
+
+  (defn price-below-pred [price]
+    #(< (:last-trade-price %) price))
+
   (defn price-range-pred [lower upper]
     #(and (> (:last-trade-price %) lower)
           (< (:last-trade-price %) upper)))
@@ -138,24 +151,74 @@
   (def alist (filter #(.isFile %) many-files))
 
 
-  (lookup alist (specific-time-pred #inst "2015-08-15T17:18:51.352-00:00"))
+  (lookupfn alist (specific-time-pred #inst "2015-08-15T17:18:51.352-00:00"))
 
-  (lookup alist (time-range-pred #inst "2015-08-15T17:18:00.000-00:00" #inst "2015-08-15T17:19:00.000-00:00"))
+  (lookupfn alist (time-range-pred #inst "2015-08-15T17:18:00.000-00:00" #inst "2015-08-15T17:19:00.000-00:00"))
 
-  (lookup alist (specific-price-pred 4.028309189176084))
+  (lookupfn alist (specific-price-pred 4.028309189176084))
 
-  (lookup alist (price-range-pred 6 10))
+  (lookupfn alist (price-range-pred 6 10))
 
   )
 
 
 ;; 4. simple query language backed by macro
 
-;;   ? lookup system to be a logic language
+(defn load-data-files [fname]
+  (filter #(.isFile %) (file-seq (io/file fname))))
 
-;;   assume data/ directory
+(defn lookup [& constraints]
+
+  ;; ensure constraints are in pairs -> Preconditions
+  {:pre [(even? (count constraints))]}
+
+  ;; map over pairs - find predicate fn based on keyword - partially apply fn with arg
+  (let [alist (if (some #{:source} constraints)
+
+                (let [source-source (comp (partial filter #(= :source (first %1)))    ;; -> a bigger use of `comp` function
+                                          (partial partition 2))
+                      source-value (comp second source-source)
+                      source-key (comp first source-source)]
+
+                  (if (string? source-key)
+                    (load-data-files (source-key constraints))
+                    source-value))
+
+                (load-data-files "data/"))
+
+        constraint-pairs (->> constraints
+                              (partition 2)
+                              (remove #(= :source (first %))))
+
+        find-lookup-fn (fn [inp]
+                         (case inp
+                           :time specific-time-pred
+                           :time-after time-after-pred
+                           :time-before time-before-pred
+                           :price specific-price-pred
+                           :price-abouve price-abouve-pred
+                           :price-below price-below-pred))
+
+        constraint-pairs-A (map (fn [x]
+                                  (update x first #(find-lookup-fn (first %))))
+                                constraint-pairs)
+
+        constraint-pairs-B (map (fn [x]
+                                  (fn [y]
+                                    ((first x) y (second x)))))
+
+        result ((apply juxt constraint-pairs-B) alist)]
+
+    ;; apply all fns with args
+
+    result))
+
+
+;; later - negation
 
 (comment
+
+  (def many-files (file-seq (io/file "data/")))
 
   (lookup :time "2015-08-15T17:18:51.352-00:00")
   (lookup :time-after "2015-08-15T17:18:00.000-00:00")
@@ -170,4 +233,11 @@
   (lookup :time-after "2015-08-15T17:18:00.000-00:00"
           :time-before "2015-08-15T17:19:00.000-00:00"
           :price-abouve 12
-          :price-below 20))
+          :price-below 20)
+
+  (lookup :source many-files
+          :time "2015-08-15T17:18:51.352-00:00")
+
+  (lookup :source "data/"
+          :time "2015-08-15T17:18:51.352-00:00")
+  )
